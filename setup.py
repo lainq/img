a243ea96e69d66e1fa04d0ad1a680e99b0605d17
pranavbaburaj <diamonddevstuff@gmail.com>
2021-10-14 11:21:46 +0530

    version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fp:
 
 setup(
     name="terminal-img",
-    version="0.0.1",
+    version="0.0.2",
     author="Pranav Baburaj",
     author_email="i.am.pranav.baburaj@gmail.com",
     url="https://github.com/pranavbaburaj/img",
